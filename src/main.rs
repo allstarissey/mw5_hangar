75290aa37fff736f51fabf5c4b270c175e3640b4
Draqon <draqonfyre@yahoo.com>
2023-07-16 21:38:18 -0500

    Updated .gitignore Added get_mechs Open data file before commands are processed List command implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(unused)]
 
-use std::path::PathBuf;
+use std::{path::PathBuf, io::{self, Read, Write}};
+use std::fs::File;
 use clap::{Parser, Subcommand};
 
 mod mech;
+
+use mech::Mech;
 
 
 #[cfg(not(debug_assertions))]
@@ -39,12 +42,40 @@ fn main() {
         args.file = Some(PathBuf::from(DEFAULT_DATA_FILE_PATH));
     }
 
+    let mut data_file = match File::open(args.file.as_ref().unwrap()) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error opening data file: {}", e);
+            return;
+        }
+    };
+
     match args.command {
         Some(c) => match c {
-            Commands::List => {},
+            Commands::List => {
+                let mechs = match get_mechs(&mut data_file) {
+                    Ok(mechs) => mechs,
+                    Err(e) => {
+                        eprintln!("Error reading data file: {}", e);
+                        return;
+                    }
+                };
+
+                for mech in mechs {
+                    println!("[{}] {}\nWeight: {} tons\nSpeed: {} km/h\n", mech.model(), mech.name(), mech.weight(), mech.speed());
+                }
+            },
             Commands::Add => {},
             Commands::Remove => {},
         },
         None => {},
     }
+}
+
+
+fn get_mechs(data_file: &mut File) -> io::Result<Vec<Mech>> {
+    let mut data = String::new();
+    data_file.read_to_string(&mut data)?;
+
+    Ok(serde_json::from_str(&data)?)
 }
