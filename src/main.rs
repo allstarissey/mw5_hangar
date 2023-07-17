d222edba3ca76b5cc0441c364ce606ea6fecafce
Draqon <draqonfyre@yahoo.com>
2023-07-16 21:48:16 -0500

    Removed commands, opting for a list flag instead

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod mech;
 use mech::Mech;
 
 
+//* Remove the following line when compiling for production
 #[cfg(not(debug_assertions))]
 const DEFAULT_DATA_FILE_PATH: &str = "data.json";
 #[cfg(debug_assertions)]
@@ -17,24 +18,13 @@ const DEFAULT_DATA_FILE_PATH: &str = "test/data.json";
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[command(subcommand)]
-    command: Option<Commands>,
-
     #[arg(short, long, value_name = "DATA FILE")]
     file: Option<PathBuf>,
+
+    #[arg(short, long, help = "Lists all mechs without launching main application")]
+    list: bool,
 }
 
-#[derive(Subcommand)]
-enum Commands {
-    #[command(about = "Lists all mechs defined in the data file")]
-    List,
-
-    #[command(about = "Adds a new mech to the data file")]
-    Add,
-
-    #[command(about = "Removes a mech from the data file")]
-    Remove,
-}
 
 fn main() {
     let mut args = Args::parse();
@@ -50,25 +40,20 @@ fn main() {
         }
     };
 
-    match args.command {
-        Some(c) => match c {
-            Commands::List => {
-                let mechs = match get_mechs(&mut data_file) {
-                    Ok(mechs) => mechs,
-                    Err(e) => {
-                        eprintln!("Error reading data file: {}", e);
-                        return;
-                    }
-                };
+    if args.list {
+        let mechs = match get_mechs(&mut data_file) {
+            Ok(mechs) => mechs,
+            Err(e) => {
+                eprintln!("Error reading data file: {}", e);
+                return;
+            }
+        };
 
-                for mech in mechs {
-                    println!("[{}] {}\nWeight: {} tons\nSpeed: {} km/h\n", mech.model(), mech.name(), mech.weight(), mech.speed());
-                }
-            },
-            Commands::Add => {},
-            Commands::Remove => {},
-        },
-        None => {},
+        for mech in mechs {
+            println!("[{}] {}\nWeight: {} tons\nSpeed: {} km/h\n", mech.model(), mech.name(), mech.weight(), mech.speed());
+        }
+
+        return;
     }
 }
 
